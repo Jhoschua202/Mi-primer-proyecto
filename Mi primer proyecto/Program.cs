6ccdbd52857b03598e0ed492cc176f5952f5e923
Jhoschua202 <jhoschua504@gmail.com>
2024-08-15 13:33:11 -0600

    actualizacion de codigos en la clase program
    Recibir y convertit datos

--- Mi primer proyecto/Program.cs
+++ Mi primer proyecto/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string Nombre;
+Console.WriteLine("Bienvenidos a nuestro primer sistema creado en C#");
+Console.WriteLine("Ingrese tu nombre para continuar");
+Nombre = Console.ReadLine();//para recibir Valores de caracter
+Console.WriteLine("Ingrese su edad");
+int Edad = Convert.ToInt32(Console.ReadLine);// convertir a dato tipo double 
+ double EdadMeses = Edad * 12; // Calculos Matematicos
+Console.WriteLine("La edad en meses es: " + EdadMeses + "Meses");//Contatenacion
